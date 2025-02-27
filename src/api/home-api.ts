2386a31a4b04b18b39ea368f1c6dfbd2893ecfc5
wonza-hub <wonza8585@gmail.com>
2025-02-27 18:12:44 +0900

    fix: 에러 처리 수정
    출석 등록 에러 응답 객체 파싱 잘못한 것 수정

--- src/api/home-api.ts
+++ src/api/home-api.ts
@@ -48,8 +48,8 @@ export function usePostMyAttendance() {
         },
         onError: async e => {
             let errorMessage = '';
-            if (isServerError(e) && e.response.data && e?.response?.data?.error.message) {
-                errorMessage = e.response.data.error.message;
+            if (isServerError(e) && e.response.data && e?.response?.data?.message) {
+                errorMessage = e.response.data.message;
                 alert(errorMessage);
 
                 return;
@@ -58,7 +58,7 @@ export function usePostMyAttendance() {
                 errorMessage = '권한이 없는 사용자입니다';
             } else if (e.response.status === 401) {
                 errorMessage = '로그인 후 다시 시도해주세요.';
-            } else if (e.response.status === 404) {
+            } else if (e.response.status === 409) {
                 errorMessage = '오늘은 이미 출석하셨습니다! 내일도 방문해주세요!';
             } else if (e.response.status === 500) {
                 errorMessage = '출석 등록에 실패하였습니다. 관리자에게 문의해주세요.';
