9100b375468a9e19ee199a2375db034f216daa34
wonza-hub <wonza8585@gmail.com>
2025-02-28 19:24:41 +0900

    style: 메인 페이지 인생네컷 에러처리 분기문 수정
    - if..else -> switch

--- src/(routes)/home/_lib/postLife4CutPhoto.ts
+++ src/(routes)/home/_lib/postLife4CutPhoto.ts
@@ -2,38 +2,45 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import isServerError from '../../../_errors/isServerError';
 
-// REST: 사진 등록
+// POST: 사진 등록
 export const usePostLife4CutPhoto = () => {
     const queryClient = useQueryClient();
+    const photoZoneURL = '/api/life4cut/save';
 
     return useMutation<AxiosResponse, AxiosError, FormData>({
-        mutationFn: async fileFormData => {
-            const photoZoneURL = '/api/life4cut/save';
-            return await axios.post(photoZoneURL, fileFormData, {
+        mutationFn: async fileFormData =>
+            await axios.post(photoZoneURL, fileFormData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
-            });
-        },
-        // 클라이언트 업데이트
+            }),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['photo-zone'] });
         },
-        // 에러 처리
         onError: async e => {
             let errorMessage = '알 수 없는 에러가 발생했습니다. 관리자에게 문의해주세요.';
-            if (isServerError(e) && e.response.data && e?.response?.data?.message) {
+
+            // 서버 에러 처리
+            if (isServerError(e) && e.response.data?.message) {
                 errorMessage = e.response.data.message;
-                if (e.response.status === 403) {
-                    errorMessage = '권한이 없는 사용자입니다.';
-                } else if (e.response.status === 401) {
-                    errorMessage = '사진은 로그인 후 올릴 수 있습니다!';
-                } else if (e.response.status === 404) {
-                    errorMessage = '사진 등록에 실패하였습니다.';
-                } else if (e.response.status === 500) {
-                    errorMessage = '사진 등록에 실패하였습니다. 관리자에게 문의해주세요.';
+
+                // 상태 코드에 따른 에러 메시지 처리
+                switch (e.response.status) {
+                    case 403:
+                        errorMessage = '권한이 없는 사용자입니다.';
+                        break;
+                    case 401:
+                        errorMessage = '사진은 로그인 후 올릴 수 있습니다!';
+                        break;
+                    case 404:
+                        errorMessage = '사진 등록에 실패하였습니다.';
+                        break;
+                    case 500:
+                        errorMessage = '사진 등록에 실패하였습니다. 관리자에게 문의해주세요.';
+                        break;
+                    default:
+                        break;
                 }
-                alert(errorMessage);
-                return;
             }
+
             alert(errorMessage);
         },
     });
