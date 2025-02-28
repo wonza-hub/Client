38486d24811517f7e8057d0dbb2c2635c8d6159d
wonza-hub <wonza8585@gmail.com>
2025-02-28 19:23:33 +0900

    refactor: 사진 게시판 게시물 등록 api 분리

--- src/(routes)/home/photo_zone/_components/PhotoPostForm.tsx
+++ src/(routes)/home/photo_zone/_components/PhotoPostForm.tsx
@@ -1,11 +1,71 @@
-// COMPONENT: 인생네컷 등록 폼
+// COMPONENT: 파일 입력 컴포넌트
 import { useState, useRef } from 'react';
+import { usePostLife4CutPhoto } from '../../_lib/postLife4CutPhoto';
+// import FileInput from './FileInput';
+// import UploadButtons from './UploadButtons';
+import { TbCameraSelfie } from 'react-icons/tb';
 import { FaCloudUploadAlt } from 'react-icons/fa';
 import { MdOutlineCancel } from 'react-icons/md';
-import { TbCameraSelfie } from 'react-icons/tb';
 import LoadingSpinner from '../../../../_components/loadingSpinner/LoadingSpinner';
-import { usePostLife4CutPhoto } from '../../_lib/postLife4CutPhoto';
 
+const FileInput = ({ file, setFile, setFileName, isPending, fileInputRef }) => {
+    const handleFileChange = ({ target: { files } }) => {
+        if (files[0]) {
+            setFileName(files[0].name);
+            setFile(files[0]);
+        }
+    };
+
+    return (
+        <div
+            onClick={() => {
+                fileInputRef.current.value = null;
+                fileInputRef.current.click();
+            }}
+            className={`${
+                file ? 'bg-primary' : 'bg-white'
+            } flex w-fit flex-row items-center justify-evenly rounded-lg border-2 border-primary p-[0.7rem] font-semibold text-primary shadow-md hover:cursor-pointer`}
+        >
+            {file ? (
+                <span className='mx-1 w-fit max-w-[20rem] truncate text-white'>{file.name}</span>
+            ) : (
+                <TbCameraSelfie size={24} />
+            )}
+            <input
+                disabled={isPending}
+                ref={fileInputRef}
+                className='hidden h-full w-full'
+                type='file'
+                accept='.jpg, .jpeg, .png'
+                onChange={handleFileChange}
+            />
+        </div>
+    );
+};
+
+// COMPONENT: 업로드 버튼 컴포넌트
+const UploadButtons = ({ file, isPending, onCancel, onSubmit }) => (
+    <>
+        {file ? (
+            <>
+                {/* 업로드 확정 */}
+                <button
+                    // onClick={e => e.stopPropagation()}
+                    className='w-fit rounded-lg border-2 border-primary bg-white px-2 text-primary'
+                    onClick={onSubmit}
+                >
+                    {isPending ? <LoadingSpinner size={20} /> : <FaCloudUploadAlt size={20} />}
+                </button>
+                {/* 업로드 취소 */}
+                <button disabled={isPending} className='w-fit rounded-lg bg-primary px-2 text-white' onClick={onCancel}>
+                    <MdOutlineCancel size={18} />
+                </button>
+            </>
+        ) : null}
+    </>
+);
+
+// COMPONENT: 사진 업로드 폼
 export default function PhotoPostForm() {
     const [file, setFile] = useState<File>(null);
     const [fileName, setFileName] = useState<string>('No selected file');
@@ -23,66 +83,29 @@ export default function PhotoPostForm() {
         setFile(null);
     };
 
+    // HANDLER: 업로드 취소
+    const handleCancel = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        setFileName(null);
+        setFile(null);
+    };
+
     return (
-        <form
-            encType='multipart/form-data'
-            onClick={() => {
-                fileInputRef.current.value = null;
-                fileInputRef.current.click();
-            }}
-            onSubmit={handlePhotoCreate}
-            className='h-fit'
-        >
-            <input
-                disabled={isPhotoPending}
-                ref={fileInputRef}
-                className=' hidden h-full w-full'
-                type='file'
-                accept='.jpg, .jpeg, .png'
-                onChange={({ target: { files } }) => {
-                    if (files[0]) {
-                        setFileName(files[0].name);
-                        setFile(files[0]);
-                    }
-                }}
+        <form encType='multipart/form-data' onSubmit={handlePhotoCreate} className='h-fit'>
+            <FileInput
+                file={file}
+                setFile={setFile}
+                setFileName={setFileName}
+                isPending={isPhotoPending}
+                fileInputRef={fileInputRef}
             />
-            {/* 상호작용 버튼 */}
             <div className='flex h-fit flex-row gap-1'>
-                <div
-                    className={`${
-                        file ? 'bg-primary' : 'bg-white'
-                    } flex w-fit flex-row items-center justify-evenly rounded-lg
-            border-2 border-primary p-[0.7rem] font-semibold text-primary shadow-md hover:cursor-pointer`}
-                >
-                    {file ? (
-                        <span className='mx-1 w-fit max-w-[20rem] truncate text-white'>{fileName}</span>
-                    ) : (
-                        <TbCameraSelfie size={24} />
-                    )}
-                </div>
-                {file ? (
-                    <>
-                        {/* 업로드 확정 */}
-                        <button
-                            onClick={e => e.stopPropagation()}
-                            className='w-fit rounded-lg border-2 border-primary bg-white px-2 text-primary'
-                        >
-                            {isPhotoPending ? <LoadingSpinner size={20} /> : <FaCloudUploadAlt size={20} />}
-                        </button>
-                        {/* 업로드 취소 */}
-                        <button
-                            disabled={isPhotoPending}
-                            className='w-fit rounded-lg bg-primary px-2 text-white'
-                            onClick={event => {
-                                event.stopPropagation();
-                                setFileName(null);
-                                setFile(null);
-                            }}
-                        >
-                            <MdOutlineCancel size={18} />
-                        </button>
-                    </>
-                ) : null}
+                <UploadButtons
+                    file={file}
+                    isPending={isPhotoPending}
+                    onCancel={handleCancel}
+                    onSubmit={handlePhotoCreate}
+                />
             </div>
         </form>
     );
--- src/(routes)/photo-album/post/page.tsx
+++ src/(routes)/photo-album/post/page.tsx
@@ -1,72 +1,37 @@
-// PAGE: 앨범 작성 페이지
+// PAGE: 앨범 게시물 등록 페이지
 import PhotoAlbumForm from '../_components/PhotoAlbumForm';
-import axios from 'axios';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IUploadedFileDto } from '../types';
-import { TIME_OUT, PAGE_ROUTE } from '../../../_constants/constants';
 import { IPhotoAlbumDescriptionValues } from '../types';
 import { SubmitHandler } from 'react-hook-form';
-import isServerError from '../../../_errors/isServerError';
+import useCreatePhotoAlbumPost from '../_lib/postPhotoAlbumPost';
 
 export default function Page() {
     const [files, setFiles] = useState<IUploadedFileDto[]>([]);
-    const [isPending, setIsPending] = useState<boolean>(false);
 
     const navigate = useNavigate();
-    // 폼 제출 핸들러
+    const { mutate: createPhotoAlbumPost, isPending: isPhotoAlbumCreationPending } = useCreatePhotoAlbumPost(navigate);
+
+    // HANDLER: 폼 제출 핸들러
     const handlePostFormSubmit: SubmitHandler<IPhotoAlbumDescriptionValues> = async data => {
         if (files.length === 0) {
             alert('사진 첨부는 필수입니다!');
+
             return;
         }
-        setIsPending(true);
-        const formData = new FormData();
+
+        const photoAlbumFormData = new FormData();
 
         // Blob로 변환 후 폼데이터에 삽입
         const descriptionBlob = new Blob([JSON.stringify(data)], {
             type: 'application/json',
         });
-        formData.append('data', descriptionBlob);
-        files.forEach(file => formData.append('file', file.file));
+        photoAlbumFormData.append('data', descriptionBlob);
+        files.forEach(file => photoAlbumFormData.append('file', file.file));
 
-        // CREATE
-        axios
-            ?.post(`/api/photo-post/post`, formData, {
-                timeout: TIME_OUT,
-                withCredentials: true,
-                headers: { 'Content-Type': 'multipart/form-data' },
-            })
-            ?.then(() => navigate(`/${PAGE_ROUTE.PHOTOALBUMS}`))
-            ?.catch(e => {
-                let errorMessage = '';
-                if (e.code === 'ECONNABORTED') {
-                    errorMessage = '요청시간을 초과하였습니다.';
-                    alert(errorMessage);
-                    return;
-                }
-                if (e.message === 'Network Error') {
-                    errorMessage = '네트워크 에러!';
-                    alert(errorMessage);
-                    return;
-                }
-                if (isServerError(e) && e.response.data && e?.response?.data?.message) {
-                    errorMessage = e.response.data.message;
-                    alert(errorMessage);
-                    return;
-                }
-                if (e.response.status === 403) {
-                    errorMessage = '권한이 없는 사용자입니다';
-                } else if (e.response.status === 401) {
-                    errorMessage = '로그인 후 다시 시도해주세요.';
-                } else if (e.response.status === 500) {
-                    errorMessage = '게시물 등록에 실패하였습니다. 관리자에게 문의해주세요.';
-                }
-                alert(errorMessage);
-            })
-            .finally(() => {
-                setIsPending(false);
-            });
+        // REST: 게시물 등록
+        createPhotoAlbumPost(photoAlbumFormData);
     };
 
     return (
@@ -76,7 +41,7 @@ export default function Page() {
                     files={files}
                     setFiles={setFiles}
                     onSubmit={handlePostFormSubmit}
-                    isPending={isPending}
+                    isPending={isPhotoAlbumCreationPending}
                 />
             </div>
         </main>
