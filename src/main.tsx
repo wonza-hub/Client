de733fbbaf8ac737b4172bec8ecbe1cb165d6558
wonza-hub <wonza8585@gmail.com>
2025-03-04 16:21:28 +0900

    chore: strict mode 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import setupLocatorUI from '@locator/runtime';
+import { StrictMode } from 'react';
 
 if (import.meta.env.DEV) {
     setupLocatorUI();
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
