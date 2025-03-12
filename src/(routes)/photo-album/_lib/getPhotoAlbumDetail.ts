d576294c695238a803dadbdaaf675d46c4ed131d
wonza-hub <wonza8585@gmail.com>
2025-03-12 13:48:22 +0900

    fix: 사진 게시판 수정 시 Suspense 사용에 따른 리액트쿼리 훅 수정
    useQuery->useSuspenseQuery

--- src/(routes)/photo-album/_lib/getPhotoAlbumDetail.ts
+++ src/(routes)/photo-album/_lib/getPhotoAlbumDetail.ts
@@ -1,17 +1,16 @@
 // GET: 앨범 단건 조회 함수
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { IPhotoAlbumData } from '../types';
 
 const useGetPhotoAlbumDetail = (boardId: string) => {
     const albumURL = `/api/photo-post/${boardId}`;
 
-    return useQuery<IPhotoAlbumData>({
+    return useSuspenseQuery<IPhotoAlbumData>({
         queryKey: ['album', boardId],
         queryFn: async () => await axios.get(albumURL).then(res => res.data.response),
         retry: false,
         refetchOnWindowFocus: false,
-        throwOnError: true,
     });
 };
 
