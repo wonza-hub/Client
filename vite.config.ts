8f4ca63a36e728b0e86bdea7c348e018294d9966
wonza-hub <wonza8585@gmail.com>
2025-04-08 23:40:07 +0900

    chore: 로컬 테스트 시 api 요청 타겟 수정

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://192.168.219.109:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
